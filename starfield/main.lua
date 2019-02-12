09d38a84b673d0c593721108f2e785aabb0684e5
Lucas Walter <wsacul@gmail.com>
2019-02-11 17:09:57 -0800

    Don't make stars green, reduce alpha on background stars, increase size of foreground moving stars #1

--- starfield/main.lua
+++ starfield/main.lua
@@ -15,11 +15,11 @@ function love.load()
     star["y"] = math.random(0.0, love.graphics.getHeight())
     size_pre = math.random() * 0.9 + 0.1
     star["size"] = size_pre * 2.0  -- * 5.5
-    star["speed"] = star["size"] * (0.05 + math.random() * 0.05)
+    star["speed"] = star["size"] * (0.2 + math.random() * 0.4)
     base = 0.85
-    star["r"] = base + math.random(0.0, 1.0 - base)
-    star["g"] = base + math.random(0.0, 1.0 - base)
-    star["b"] = base + math.random(0.0, 1.0 - base)
+    star["r"] = base + math.random() * (1.0 - base)
+    star["g"] = base + math.random() * (1.0 - base) * 0.5
+    star["b"] = base + math.random() * (1.0 - base)
     brightness = math.random()  -- math.random(0.2, 0.3) + size_pre * 0.2
     -- star["r"] = star["r"] * brightness
     -- star["g"] = star["g"] * brightness
@@ -27,9 +27,11 @@ function love.load()
     star["a"] = brightness
 
     if i < 1500 then
+      star["size"] = star["size"] + 1.0
+      star["a"] = star["a"] + 0.3
       table.insert(stars, star)
     else
-      love.graphics.setColor(star["r"], star["g"], star["b"], star["a"])
+      love.graphics.setColor(star["r"], star["g"], star["b"], star["a"] * 0.9)
       -- love.graphics.circle("fill", v["x"], v["y"], v["size"] * 1.6)
       -- love.graphics.setColor(v["r"], v["g"], v["b"], v["a"] * 0.5)
       love.graphics.circle("fill", star["x"], star["y"], star["size"])
