b3378407601a84e01b5c5d8d0d1ed9aaeb6d28ec
Lucas Walter <wsacul@gmail.com>
2019-02-11 16:55:48 -0800

    math.random(min max) doesn't work as expected (also don't know how to print to a console to see what it is doing, but math.random() does reliably produce a 0.0-1.0 that then can be offset and scaled #1

--- starfield/main.lua
+++ starfield/main.lua
@@ -5,30 +5,40 @@ stars = {}
 
 function love.load()
   love.window.setTitle('endless side scrolling stars')
+  love.window.setFullscreen(true)
+  canvas = love.graphics.newCanvas(love.graphics.getWidth(), love.graphics.getHeight())
+  love.graphics.setCanvas(canvas)
 
-  for i=1,1000,1 do
+  for i=1,25000,1 do
     star = {}
-    star["x"] = math.random(0.0, love.graphics.getWidth())
+    star["x"] = math.random(0.0, love.graphics.getWidth() + 20)
     star["y"] = math.random(0.0, love.graphics.getHeight())
-    size_pre  = math.random(0.2, 2.8)
-    star["size"] = size_pre  -- * 5.5
-    star["speed"] = star["size"] * (0.05 + math.random(0, 0.1))
-    base = 0.9
+    size_pre = math.random() * 0.9 + 0.1
+    star["size"] = size_pre * 2.0  -- * 5.5
+    star["speed"] = star["size"] * (0.05 + math.random() * 0.05)
+    base = 0.85
     star["r"] = base + math.random(0.0, 1.0 - base)
     star["g"] = base + math.random(0.0, 1.0 - base)
     star["b"] = base + math.random(0.0, 1.0 - base)
-    brightness = math.random(0.2, 0.5) + size_pre * 0.2
+    brightness = math.random()  -- math.random(0.2, 0.3) + size_pre * 0.2
     -- star["r"] = star["r"] * brightness
     -- star["g"] = star["g"] * brightness
     -- star["b"] = star["b"] * brightness
     star["a"] = brightness
 
-    print("test")  -- star["a"])
+    if i < 1500 then
+      table.insert(stars, star)
+    else
+      love.graphics.setColor(star["r"], star["g"], star["b"], star["a"])
+      -- love.graphics.circle("fill", v["x"], v["y"], v["size"] * 1.6)
+      -- love.graphics.setColor(v["r"], v["g"], v["b"], v["a"] * 0.5)
+      love.graphics.circle("fill", star["x"], star["y"], star["size"])
+    end
 
-    table.insert(stars, star)
+    -- print("test")  -- star["a"])
+
   end
-  -- canvas = love.graphics.newCanvas(love.window.getWidth(), love.window.getHeight())
-  -- canvas = love.graphics.newCanvas(800, 800)
+  love.graphics.setCanvas()
 end
 
 function love.draw()
@@ -37,18 +47,21 @@ function love.draw()
     -- love.graphics.setCanvas(canvas)
     -- love.graphics.line(old_x, old_y, x, y)
     -- love.graphics.setCanvas()
-    -- love.graphics.draw(canvas, 0, 0)
-    for k, v in pairs(stars) do
-      v["x"] = v["x"] - v["speed"]
-      if v["x"] < -2 then
-        v["x"] = love.graphics.getWidth() + 20
-        v["y"] = math.random(0.0, love.graphics.getHeight())
-      end
-      love.graphics.setColor(v["r"], v["g"], v["b"], v["a"])
-      -- love.graphics.circle("fill", v["x"], v["y"], v["size"] * 1.6)
-      -- love.graphics.setColor(v["r"], v["g"], v["b"], v["a"] * 0.5)
-      love.graphics.circle("fill", v["x"], v["y"], v["size"])
+    love.graphics.setColor(1.0, 1.0, 1.0, 1.0)
+    love.graphics.draw(canvas, 0, 0)
+    if true then
+      for k, v in pairs(stars) do
+        v["x"] = v["x"] - v["speed"]
+        if v["x"] < -2 then
+          v["x"] = love.graphics.getWidth() + 20
+          v["y"] = math.random(0.0, love.graphics.getHeight())
+        end
+        love.graphics.setColor(v["r"], v["g"], v["b"], v["a"])
+        -- love.graphics.circle("fill", v["x"], v["y"], v["size"] * 1.6)
+        -- love.graphics.setColor(v["r"], v["g"], v["b"], v["a"] * 0.5)
+        love.graphics.circle("fill", v["x"], v["y"], v["size"])
 
+      end
     end
     -- love.graphics.print("foo bar", x, y)
 
